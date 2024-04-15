226758558576761c32cdea8b9ab1d3bcb08ab3cd
TuanTD <tuantd.6698@gmail.com>
2024-04-15 11:29:54 +0700

    commit test

--- src/test/java/core/common/Constant.java
+++ src/test/java/core/common/Constant.java
@@ -8,10 +8,7 @@ public class Constant {
     public static final Integer LONG_TIMEOUT = 90;
     public static final Integer MEDIUM_TIMEOUT = 30;
     public static final Integer SHORT_TIMEOUT = 10;
-    public static final String IP = "192.168.1.8";
-    public static final String AUT_URL = "https://login.salesforce.com/";
-    public static final String SALESFORCE_REPOSITORY = "SampleRepository";
-    public static final String SALESFORCE_NAME = "caohai0707@gmail.com";
-    public static final String SALESFORCE_PASSWORD = "123@123a";
+
+
 
 }
--- src/test/java/core/common/TestBase.java
+++ src/test/java/core/common/TestBase.java
@@ -5,7 +5,6 @@ import org.testng.annotations.BeforeMethod;
 
 import static core.common.BuiltInAction.maximizeWindow;
 import static core.common.BuiltInAction.navigateTo;
-import static core.common.Constant.AUT_URL;
 import static core.locators.WebDriver.openBrowser;
 import static core.common.BuiltInAction.quit;
 
